b2bebb92cbb15b5dd655cb12f21a76d8f1928a1c
50tc1v <50tc1v@proton.me>
2023-02-20 18:25:20 -0300

    arquivos de config do nvim adicionado.

---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,22 @@
+-- General setup
+vim.opt.mouse = 'a'
+vim.opt.swapfile = false
+vim.opt.history = 100
+vim.opt.lazyredraw = true
+
+-- Theme
+vim.opt.termguicolors = true
+vim.cmd [[ colorscheme monokai ]]
+vim.g.transparent_enabled = true
+
+-- Editor
+vim.opt.number = true
+vim.opt.relativenumber  = true
+vim.opt.wrap = false
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.list = true
---
+++ .config/nvim/lua/vim-plug.lua
@@ -0,0 +1,10 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+Plug 'ap/vim-css-color'
+Plug 'tanvirtin/monokai.nvim'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'xiyaowong/nvim-transparent'
+Plug 'ryanoasis/vim-devicons'
+vim.call('plug#end')
